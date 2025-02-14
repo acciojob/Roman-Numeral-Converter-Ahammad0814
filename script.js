a92c87a821657d54434c27e38520873e4d1cbdb0
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2025-02-14 11:32:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,21 @@ function convertToRoman(num) {
     };
 
   //your code here
+let result = '';
+
+    // Loop through each Roman numeral value, starting from the largest to smallest
+    for (let i = 0; i < 7; i++) {
+        const [roman, value] = obj[i];
+        
+        // While the number is greater than or equal to the current value
+        while (num >= value) {
+            result += roman;  // Append the Roman numeral
+            num -= value;     // Subtract the value from num
+        }
+    }
+
+    return result;
+	
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
